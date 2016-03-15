49c542550c9f1b4260ebca85f157e6bf2bb32e87
dtkenyon <dtkenyon@bu.edu>
2016-03-15 00:12:50 -0400

    CSV_Writer
    I'm having trouble running/debugging the main program. I am attempting
    to call the function inside the CSV_Writer class which is supposed to
    write two lines to a text file.

---
+++ Data Processing/CSV_Writer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team_E_FIRE_Data_Processing_Program_BBB
+{
+    class CSV_Writer
+    {
+        //string winDir = System.Environment.GetEnvironmentVariable("windir");
+        public void WriteStrikeToCSV()
+        {
+            string s = "Hello";
+            //a TextWriter writes characters to a stream; can be used to open as well
+            StreamWriter writer = new StreamWriter("C:\\EfireData"); 
+            writer.WriteLine("<strike #>, <diode struck>, <electron energy>, <strike time>");
+            File.AppendText(s);
+            writer.Close();
+            //Console.ReadLine();
+        }
+    }
+}
