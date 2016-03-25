4f38a237d2cef9af7c06d7f3f00a3461cf2dace6
jmarcao <jmarcao@bu.edu>
2016-03-25 12:15:44 -0400

    SPI Functionality
    Added SPI functionality, but still needs bug-fixing and testing.

--- MIROC2 Driver/pru/arm_clk.c
+++ MIROC2 Driver/pru/arm_clk.c
@@ -7,9 +7,35 @@ This file is used to allocate memory to the PRU and enable it.
 #include <stdlib.h>
 #include <prussdrv.h>
 #include <pruss_intc_mapping.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <stdint.h>
+#include <linux/spi/spidev.h>
+
+#define SPI_PATH "/dev/spidev1.0"
 
 #define PRU_0	0
 #define PRU_1 1
+
+int transfer(int fd, unsigned char send[], unsigned char recieve[], int length){
+  // Establish Trasfer characteristics
+  struct spi_ioc_transfer transfer;
+  transfer.tx_buf = (unsigned long) send;
+  transfer.rx_buf = (unsigned long) recieve;
+  transfer.len = length;
+  transfer.speed_hz = 10000;
+  transfer.bits_per_word = 8;
+  transfer.delay_usecs = 0;
+
+  // Send the SPI Message
+  int status = ioctl(fd, SPI_IOC_MESSAGE(1), &transfer);
+  if (status < 0){
+    perror("SPI: SPI_IOC_MESSAGE failed");
+    return -1;
+  }
+  return status;
+}
 
 int main (void)
 {
@@ -18,25 +44,55 @@ int main (void)
       exit(EXIT_FAILURE);
    }
 
-   // Initialize structure used by prussdrv_pruintc_intc
-   // PRUSS_INTC_INITDATA is found in pruss_intc_mapping.h
-   tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
+   // Initalize SPI 
+   int fd;                        //file handle and loop counter
+   uint8_t bits = 8, mode = 3;             //8-bits per word, SPI mode 3
+   uint32_t speed = 1000000; 
+   // The following calls set up the SPI bus properties
+   if ((fd = open(SPI_PATH, O_RDWR))<0){
+      perror("SPI Error: Can't open device.");
+      return -1;
+   }
+   if (ioctl(fd, SPI_IOC_WR_MODE, &mode)==-1){
+      perror("SPI: Can't set SPI mode.");
+      return -1;
+   }
+   if (ioctl(fd, SPI_IOC_RD_MODE, &mode)==-1){
+      perror("SPI: Can't get SPI mode.");
+      return -1;
+   }
+   if (ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits)==-1){
+      perror("SPI: Can't set bits per word.");
+      return -1;
+   }
+   if (ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits)==-1){
+      perror("SPI: Can't get bits per word.");
+      return -1;
+   }
+   if (ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed)==-1){
+      perror("SPI: Can't set max speed HZ");
+      return -1;
+   }
+   if (ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed)==-1){
+      perror("SPI: Can't get max speed HZ.");
+      return -1;
+   }
 
-   // Allocate and initialize memory
+   // Initialize PRU structures
+   tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
    prussdrv_init ();
    prussdrv_open (PRU_EVTOUT_0);
 
-  //Pointer to the PRU Data memory. Unsure if this is working properly.
-  unsigned int *pruDataMem_0;
+  // Establish pointers to PRU Memory
+  unsigned char *pruDataMem_0;
   prussdrv_map_prumem (0, &pruDataMem_0);
   if (pruDataMem_0 == NULL){
     printf("Data Memory not mapped!\nExiting.");
     exit(EXIT_FAILURE);
-  } else {
-    printf("PRU_0 Data Mem starts at: %p \n", pruDataMem_0);
   }
-
-  unsigned char *pruAddrMem_0 = pruDataMem_0 + 1;
+  unsigned char *pruAddrMem_0 = pruDataMem_0 + 4; // shifted by 4 bytes
+  unsigned char *pruMem[] = {pruDataMem_0, (pruAddrMem_0 + 1), pruAddrMem_0};
+  unsigned char *recieve[] = {0,0,0};
 
    // Map PRU's interrupts
    prussdrv_pruintc_init(&pruss_intc_initdata);
@@ -46,6 +102,11 @@ int main (void)
    prussdrv_exec_program (PRU_1, "./pru_1_clk.bin");
    printf("EBB fixed-frequency clock PRU program now running\n");
 
+   printf("SPI Mode is: %d\n", mode);
+   printf("SPI Bits is: %d\n", bits);
+   printf("SPI Speed is: %d\n", speed);
+   printf("PRU_0 Data Mem starts at: %p \n", pruDataMem_0);
+
    int i;
    for(i = 0;; i++){
     // Data Collection Loop
@@ -54,12 +115,12 @@ int main (void)
     prussdrv_pru_clear_event (PRU_EVTOUT_0, PRU0_ARM_INTERRUPT);
     printf("Value: %d\n", *pruDataMem_0);
     printf("Channel: 0x%X%.2X\n", *(pruAddrMem_0 + 1), *(pruAddrMem_0));
+    transfer(fd, *pruMem, *recieve, 3);
+    // To fix: You need to combine the values for the address
    }
 
-  // int n = prussdrv_pru_wait_event (PRU_EVTOUT_0);  // This assumes the PRU generates an interrupt connected to event out 0 immediately before halting
-  // printf("PRU program completed, event number %d.\n", n);
-
    prussdrv_pru_disable(PRU_0);
+   prussdrv_pru_disable(PRU_1);
    prussdrv_exit ();
    return EXIT_SUCCESS;
 }
